6ee2f46eb4dda49cc53c0a18b32974106ec577f3
noelle <noelle.schenk@orss.ch>
2019-11-06 15:08:13 +0100

    included error messages in tests

--- tests/testthat/test-plot_GDM.R
+++ tests/testthat/test-plot_GDM.R
@@ -4,4 +4,9 @@ test_that("define_rownames_lui_or_components functions produces expected results
   expect_equal(define_rownames_lui_or_components("components"), 
                c(paste(c("G", "M", "F"), "std", sep = ""), paste("delta", c("Gstd", "Mstd", "Fstd"), sep = ""),
                  "soil", "isolation", "geo"))
+  expect_error(define_rownames_lui_or_components("luo"), "Error: input is not either lui or components.", fixed = T)
+})
+
+test_that("read_in_gdm_input_file function", {
+  expect_equal(nrow(read_in_gdm_input_dataset(pathtoout, "lui")), 5807)
 })
