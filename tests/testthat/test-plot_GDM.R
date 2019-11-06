6c4e74e4cef3aaace79fb3b7819918f473b6788b
noelle <noelle.schenk@orss.ch>
2019-11-06 13:46:09 +0100

    produced first test for function define_rownames_lui_or_components

--- tests/testthat/test-plot_GDM.R
+++ tests/testthat/test-plot_GDM.R
@@ -1,5 +1,7 @@
-test_that("plot_GDM script produces reliable input data tables", {
-  luiinput <- "lui"
-  name <- "gdm_EFturnover_0.75_LUI"
-  
+test_that("define_rownames_lui_or_components functions produces expected results", {
+  expect_equal(define_rownames_lui_or_components("lui"), 
+               c("LUI", "deltaLUI", "soil", "isolation", "geo"))
+  expect_equal(define_rownames_lui_or_components("components"), 
+               c(paste(c("G", "M", "F"), "std", sep = ""), paste("delta", c("Gstd", "Mstd", "Fstd"), sep = ""),
+                 "soil", "isolation", "geo"))
 })
