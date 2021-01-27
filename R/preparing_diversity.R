b7296e50c656396d8c55b6b1f20403dc6501593e
noelle <noelle.schenk@orss.ch>
2021-01-27 13:40:15 +0100

    added subfunction for add_back_missing_plots_species_combinations which tests if all plots are included in the input dataset and if no, adds the minimal amount of data in order for them to be included. Results in full dataset with all previously taken out zeroes introduced back.

--- R/preparing_diversity.R
+++ R/preparing_diversity.R
@@ -9,9 +9,14 @@
 #' see https://github.com/biodiversity-exploratories-synthesis/Synthesis-dataset-manual/blob/main/Synthesis%20datasets%20%20How%20to%20use.md
 #' Note : Only the columns Plot and Species are considered, any other columns will be filled with NA.
 #' This can cause problems if the dataset is later filtered for e.g. Year.
+#' Note : special case if the input dataset does not contain all Plots, if certain plots do not 
+#' contain any species. This special case is detected and solved in this function.
 #' 
 #' @param diversity_dataset A data.table of soilfuni or bacteria in the format of the synthesis diversity 
 #' dataset, with at least the columns Plot, Species and value.
+#' #' @param all_plots a character vector containing the complete set of plots from the 
+#' Exploratories. (AEG 1-50, HEG 1-50 and SEG 1-50). Is needed for the subfunction
+#' find_and_supplement_missing_plots.
 #' @return A data.table in the same format as diversity_dataset, but with the previously
 #' missing plots x species added back as 0. Note : Only the columns Plot, Species and value are
 #' considered, any other columns will be filled with NA.
@@ -26,6 +31,11 @@ add_back_missing_plots_species_combinations <- function(diversity_dataset){
     stop("The provided dataset is not a data.table. Please first convert it to data.table with
          data.table(diversity_dataset).")
   }
+  # check if the dataset contains all plots, or if plots need to be added manually
+  if(length(unique(diversity_dataset$Plot)) < 150){ 
+    diversity_dataset <- find_and_supplement_missing_plots(diversity_dataset, all_plots)
+  }
+  
   # add back zeros
   diversity_dataset <- data.table::setDT(diversity_dataset)[data.table::CJ(Species = Species, Plot = Plot, unique = T), on=.(Species, Plot)]
   print(paste(nrow(diversity_dataset[is.na(value)]), "missing combinations were added."))
@@ -34,24 +44,45 @@ add_back_missing_plots_species_combinations <- function(diversity_dataset){
       # at first attempt (only at the second). https://stackoverflow.com/questions/32988099/data-table-objects-assigned-with-from-within-function-not-printed
   return(diversity_dataset)
 }
-
-#TODO : add this part to the adding plots
-#  note : needs all_plots, a vector containing the whole list of explo plots.
-length(unique(diversity_dataset$Plot)) # if > 0, not all plots included
-# adding rows with the missing plots : 
-# any random species
-# value is 0
-missing_plots <- all_plots[!all_plots %in% unique(diversity_dataset$Plot)]
-random_species <- diversity_dataset$Species[1]
-complementary_dataset <- data.table(Plot = missing_plots, Species = random_species, value = 0)
-
-# add the complementary dataset to the original one
-diversity_dataset <- rbindlist(list(diversity_dataset, complementary_dataset), fill = T, use.names = T)
-diversity_dataset <- add_back_missing_plots_species_combinations(diversity_dataset)
-
-length(unique(diversity_dataset$Plot))
+NULL
 
 
+#' Fill number of plots to 150
+#' 
+#' Subfunction for add_back_missing_plots_species_combinations
+#' A dataset from soilfungi or bacteria can miss some plots. This happens if there are plots
+#' without any species (e.g. for symbiont.soilfungi in 2011).
+#' The dataset needs to be extended with the missing plots in order to use the function
+#' `add_back_missing_plots_species_combinations`, because this function only adds the missing
+#' plot x species combination, not completely missing plots.
+#' 
+#' @param diversity_dataset a species diversity data.table in the format of the
+#' exploratories synthesis diversity dataset. It contains at least the columns Plot, 
+#' Species and value.
+#' @param all_plots a character vector containing the complete set of plots from the 
+#' Exploratories. (AEG 1-50, HEG 1-50 and SEG 1-50).
+#' @return a data.table consisting of the unchanged diversity_dataset as it has been provided 
+#' as input, rbinded to a minimal data.table containing a random species and the 
+#' missing plots.
+find_and_supplement_missing_plots <- function(diversity_dataset, all_plots){
+  if(length(unique(diversity_dataset$Plot)) == 150){
+    stop("The dataset already contains 150 plots, no need to supplement missing ones.")
+  }
+  # find the set of missing plots and store to vector
+  # get random species to be supplemented (only 1 is needed, all missing combinations
+  # will be supplemented by add_back_missing_plots_species_combinations)
+  # create a supplement dataset
+  missing_plots <- all_plots[!all_plots %in% unique(diversity_dataset$Plot)]
+  random_species <- diversity_dataset$Species[1]
+  complementary_dataset <- data.table::data.table(Plot = missing_plots, Species = random_species, value = 0)
+  
+  diversity_dataset <- rbindlist(list(diversity_dataset, complementary_dataset), fill = T, use.names = T)
+  # check if number of plots is the expected 150 now
+  if(length(unique(diversity_dataset$Plot)) != length(all_plots)){
+    stop("The number of plots is not the expected number. Check length of vector all_plots.")
+  }
+  return(diversity_dataset)
+}
 NULL
 
 
