125c3d77ebceb601599c38a0263cef0429bcf217
noelle <noelle.schenk@orss.ch>
2022-05-16 17:08:45 +0200

    MILESTONE : analysis_nonpublic.R divided into sections of datasets to be loaded together

--- vignettes/analysis_nonpublic.R
+++ vignettes/analysis_nonpublic.R
@@ -1,5 +1,24 @@
 # This is the nonpublic file for the analysis
 
+# Usage : 
+# - in header of each script, the variables needed to be loaded are specified (elements of vector sections_to_be_loaded)
+#    save the analysis_nonpublic.R with the analysis and source it with this specified vector in order to load the
+#    needed variables
+
+# please specify the vector of wanted datasets. The following vector will load all datasets.
+# sections_to_be_loaded <- c("assemble_covariates", 
+#                            "assemble_functions", 
+#                            "imputed_functions", 
+#                            "functions_dissimilarity", 
+#                            "prepared_covariates",
+#                            "raw_diversity",
+#                            "betadiversity",
+#                            "raw_lui",
+#                            "LUI",
+#                            "additional",
+#                                "master_diversity_alpha_beta_gamma",
+#                                "to_be_specified") #TODO check if needed
+
 # PACKAGE
 # load functions from the package
 devtools::load_all()
@@ -31,66 +50,89 @@ colorder_gdm_input <- readRDS(paste(pathtodata, "/analysis/helper_data/colorder_
 ######################
 # CONSTRUCTION OF ASSEMBLED DATA FROM RAW
 ######################
-# # COVARIATES
-# small_glsoil <- data.table::fread(paste(pathtodata, "/data_assembly/raw_data/BE env covariates.txt", sep=""))
-# main_glsoil <- data.table::fread(paste(pathtodata, "/data_assembly/raw_data/Grassland EP soil descriptors.txt", sep=""))
-# names_gl <- data.table::fread(paste(pathtodata, "/data_assembly/raw_data/20826_plotNames.txt", sep=""))
-# plt.sur <- data.table::fread(paste(pathtodata, "/data_assembly/raw_data/18148_landscape.txt", sep=""))
-# geodist <- data.table::fread(paste(pathtodata, "/data_assembly/raw_data/20907.txt", sep=""))
+if("assemble_covariates" %in% sections_to_be_loaded){
+  # COVARIATES
+  small_glsoil <- data.table::fread(paste(pathtodata, "/data_assembly/raw_data/BE env covariates.txt", sep=""))
+  main_glsoil <- data.table::fread(paste(pathtodata, "/data_assembly/raw_data/Grassland EP soil descriptors.txt", sep=""))
+  names_gl <- data.table::fread(paste(pathtodata, "/data_assembly/raw_data/20826_plotNames.txt", sep=""))
+  plt.sur <- data.table::fread(paste(pathtodata, "/data_assembly/raw_data/18148_landscape.txt", sep=""))
+  geodist <- data.table::fread(paste(pathtodata, "/data_assembly/raw_data/20907.txt", sep=""))
+}
 
-
-# FUNCTIONS
-# store raw grassland functions for gapfilling
-# raw_grlfuns <- data.table::fread(paste(pathtoexplosynthesis, "/Grassland_functions/27087_grassland_functions_bexis/27087_25_Dataset/bexis_to_wide_format_output.csv", sep = ""), header=T)
-# # #TODO : in first functions script : run new reformatting script
-# # selection of required functions for gapfilling
-# selection <- info_data[`used for Gapfilling` == "yes", ColumnName]
-# raw_grlfuns <- raw_grlfuns[, ..selection]
-# rm(selection)
-
+if("assemble_functions" %in% sections_to_be_loaded){
+  # FUNCTIONS
+  # store raw grassland functions for gapfilling
+  raw_grlfuns <- data.table::fread(paste(pathtoexplosynthesis, "/Grassland_functions/27087_grassland_functions_bexis/27087_25_Dataset/bexis_to_wide_format_output.csv", sep = ""), header=T)
+  #TODO : in first functions script : run new reformatting script
+  # selection of required functions for gapfilling
+  selection <- info_data[`used for Gapfilling` == "yes", ColumnName]
+  raw_grlfuns <- raw_grlfuns[, ..selection]
+  rm(selection)
+}
 
 
 ######################
 # ASSEMBLED DATA INPUT FOR ANALYSIS
 ######################
 
-# IMPUTED FUNCTIONS
-# imputed_grlfuns <- readRDS(paste(pathtodata, "/data_assembly/output_data/imputed_function_values.rds", sep = ""))
+if("imputed_functions" %in% sections_to_be_loaded){
+  # IMPUTED FUNCTIONS
+  imputed_grlfuns <- readRDS(paste(pathtodata, "/data_assembly/output_data/imputed_function_values.rds", sep = ""))
+}
 
-# FUNCTIONAL DISSIMILARITY
-EFmaster <- readRDS(paste(pathtodata, "/data_assembly/output_data/EFmaster.rds", sep = ""))
-# EFmaster <- readRDS(paste(pathtodata, "/data_assembly/output_data/singleEFdist.rds", sep = "")) # single functions, already scaled
-# EFmaster <- readRDS(paste(pathtodata, "/data_assembly/output_data/EFbeta_abund.rds", sep = "")) # EFbeta abund
-# EFmaster <- readRDS(paste(pathtodata, "/data_assembly/output_data/EFquantile_median.rds", sep = "")) # EFbeta abund
-# EFmaster <- readRDS(paste(pathtodata, "/data_assembly/output_data/EFmaster_all_thresholds.rds", sep = ""))
+if("functions_dissimilarity" %in% sections_to_be_loaded){
+  # FUNCTIONS DISSIMILARITY
+  EFmaster <- readRDS(paste(pathtodata, "/data_assembly/output_data/EFmaster.rds", sep = ""))
+  # EFmaster <- readRDS(paste(pathtodata, "/data_assembly/output_data/singleEFdist.rds", sep = "")) # single functions, already scaled
+  # EFmaster <- readRDS(paste(pathtodata, "/data_assembly/output_data/EFbeta_abund.rds", sep = "")) # EFbeta abund
+  # EFmaster <- readRDS(paste(pathtodata, "/data_assembly/output_data/EFquantile_median.rds", sep = "")) # EFbeta abund
+  # EFmaster <- readRDS(paste(pathtodata, "/data_assembly/output_data/EFmaster_all_thresholds.rds", sep = ""))
+}
 
+if("prepared_covariates" %in% sections_to_be_loaded){
+  ## PREPARED COVARIATES
+  predictors <- readRDS(paste(pathtodata, "/data_assembly/output_data/predictors.rds", sep = ""))
+  predictors_soil <- readRDS(paste(pathtodata, "/data_assembly/output_data/predictors_soil.rds", sep = ""))
+}
 
-## PREPARED COVARIATES
-predictors <- readRDS(paste(pathtodata, "/data_assembly/output_data/predictors.rds", sep = ""))
-predictors_soil <- readRDS(paste(pathtodata, "/data_assembly/output_data/predictors_soil.rds", sep = ""))
+if("raw_diversity" %in% sections_to_be_loaded){
+  # DIVERSITY
+  spdiv <- data.table::fread(paste(pathtodata, "/data_assembly/raw_data/201124_EP_species_diversity_GRL.txt", sep=""))
+  spinfo <- data.table::fread(paste(pathtodata, "/data_assembly/raw_data/210112_EP_species_info_GRL.txt", sep=""))
+  names_trlevel <- data.table::fread(paste(pathtodata, "/data_assembly/helper_data/trophic_level_renaming.csv", sep = ""))
+}
 
-# DIVERSITY
-spdiv <- data.table::fread(paste(pathtodata, "/data_assembly/raw_data/201124_EP_species_diversity_GRL.txt", sep=""))
-spinfo <- data.table::fread(paste(pathtodata, "/data_assembly/raw_data/210112_EP_species_info_GRL.txt", sep=""))
-names_trlevel <- data.table::fread(paste(pathtodata, "/data_assembly/helper_data/trophic_level_renaming.csv", sep = ""))
+if("betadiversity" %in% sections_to_be_loaded){
+  # BETADIVERSITY
+  trlevels <- readRDS(paste(pathtodata, "/data_assembly/output_data/trlevels_vector.rds", sep = ""))
+  # masterbeta <- readRDS(paste(pathtodata, "/data_assembly/output_data/master_beta.rds", sep = ""))
+  # masterbeta <- readRDS(paste(pathtodata, "/data_assembly/output_data/master_beta_incl_poll_and_tertcons.rds", sep = ""))
+  masterbeta <- readRDS(paste(pathtodata, "/data_assembly/output_data/master_beta_no_bats_with_birds.rds", sep = "")) # THIS WAS ACTIVE LAST TIME
+  # masterbeta <- readRDS(paste(pathtodata, "/data_assembly/output_data/master_beta_abundance.rds", sep = "")) # note: needs some special modifications in scripts
+}
 
-# BETADIVERSITY
-trlevels <- readRDS(paste(pathtodata, "/data_assembly/output_data/trlevels_vector.rds", sep = ""))
-# masterbeta <- readRDS(paste(pathtodata, "/data_assembly/output_data/master_beta.rds", sep = ""))
-# masterbeta <- readRDS(paste(pathtodata, "/data_assembly/output_data/master_beta_incl_poll_and_tertcons.rds", sep = ""))
-masterbeta <- readRDS(paste(pathtodata, "/data_assembly/output_data/master_beta_no_bats_with_birds.rds", sep = "")) # THIS WAS ACTIVE LAST TIME
-# masterbeta <- readRDS(paste(pathtodata, "/data_assembly/output_data/master_beta_abundance.rds", sep = "")) # note: needs some special modifications in scripts
+if("raw_lui" %in% sections_to_be_loaded){
+  # LUI
+  rawlui <- data.table::fread(paste(pathtodata, "/data_assembly/raw_data/25086_Input Data for LUI Calculation Tool  of all grassland plots since 2006 - revised 2019_1.1.0/25086.txt", sep = ""), header=T)
+}
+if("LUI" %in% sections_to_be_loaded){
+  # LUI
+  LUI <- readRDS(paste(pathtodata, "/data_assembly/output_data/LUI.rds", sep = ""))
+  distLUI <- readRDS(paste(pathtodata, "/data_assembly/output_data/distLUI.rds", sep = ""))
+}
 
-# LUI
-# rawlui <- data.table::fread(paste(pathtodata, "/data_assembly/raw_data/25086_Input Data for LUI Calculation Tool  of all grassland plots since 2006 - revised 2019_1.1.0/25086.txt", sep = ""), header=T)
-LUI <- readRDS(paste(pathtodata, "/data_assembly/output_data/LUI.rds", sep = ""))
-distLUI <- readRDS(paste(pathtodata, "/data_assembly/output_data/distLUI.rds", sep = ""))
-
-
-
-# ADDITIONAL
-# masterdiversity <- readRDS(paste(pathtodata, "/data_assembly/output_data/master_diversity_alpha_beta.rds", sep = ""))
-masterdiversity <- readRDS(paste(pathtodata, "/data_assembly/output_data/master_diversity_alpha_beta_gamma.rds", sep = ""))
-# note that for the above masterdiv : 2 columns with plot names are given, so is alphadiv given in 2 columns.
-# master_alpha <- readRDS(paste(pathtodata, "/data_assembly/output_data/master_alpha.rds", sep = ""))
-# master_gamma <- ... "master_gamma.rds"
+if("additional" %in% sections_to_be_loaded){
+  
+  if("master_diversity_alpha_beta_gamma" %in% sections_to_be_loaded){
+    masterdiversity <- readRDS(paste(pathtodata, "/data_assembly/output_data/master_diversity_alpha_beta_gamma.rds", sep = ""))
+  }
+  
+  if("to_be_specified" %in% sections_to_be_loaded){
+    # ADDITIONAL
+    # masterdiversity <- readRDS(paste(pathtodata, "/data_assembly/output_data/master_diversity_alpha_beta.rds", sep = ""))
+    masterdiversity <- readRDS(paste(pathtodata, "/data_assembly/output_data/master_diversity_alpha_beta_gamma.rds", sep = ""))
+    # note that for the above masterdiv : 2 columns with plot names are given, so is alphadiv given in 2 columns.
+    # master_alpha <- readRDS(paste(pathtodata, "/data_assembly/output_data/master_alpha.rds", sep = ""))
+    # master_gamma <- ... "master_gamma.rds"
+  }
+}
