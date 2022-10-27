29f364f377547805b0e22ce37cf21591473886b3
noelle <noelle.schenk@orss.ch>
2022-10-27 10:07:41 +0200

    added loading requirements and description of procedure and conclusions from this sensitivity analysis

--- vignettes/calc_multifun_from_imputed_additional_sensitivity.R
+++ vignettes/calc_multifun_from_imputed_additional_sensitivity.R
@@ -8,6 +8,16 @@
 #
 # also contains useful pairs() function.
 #
+# load requirements
+sections_to_be_loaded <- c()
+source("vignettes/analysis_nonpublic.R")
+
+# note : the below file was created by changing the number of selected PCA axes to
+#    11, 13 and 14 and saving the output into a data.table.
+#    in order to reproduce, use the file "calc_multifun_from_imputed.Rds" and
+#    run everything until the pca, until the line : 
+#    "saveRDS(EFmaster, "EFdistance.rds")" and instead of saving, keep the data.table
+#    and rerun the above section with a different number than 11.
 comparison <- readRDS(paste(pathtodata, "/data_assembly/output_data/21-03-10_calc_multifun_from_imputed_sensitivity_pca_axes_selection_impact_of_number_of_axes_on_EFdistance.RDS", sep = ""))
 
 plot(comparison$EFdistance_11_axes, comparison$EFdistance_14_axes,
@@ -36,3 +46,8 @@ panel.cor <- function(x, y, digits = 2, prefix = "", cex.cor, ...)
 pairs(comparison[, .(EFdistance_11_axes, EFdistance_13_axes, EFdistance_14_axes)],
       diag.panel = panel.hist,
       upper.panel = panel.cor)
+
+# result : the EFdist measures with 13 or 14 instead of 11 axes are highly correlated with
+#    each other c > 0.98
+#    note (limitation) : high number of replicates, therefore higher correlation expected.
+# conclusion : the EFdist measure is robust to a deviating number of PC axes selected.
