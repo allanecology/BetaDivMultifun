9931e11befeeae23d9d51336a178bee536316381
noelle <noelle.schenk@orss.ch>
2022-11-16 14:22:58 +0100

    added function to source rmd file

--- R/setting_environment.R
+++ R/setting_environment.R
@@ -25,3 +25,22 @@ setNonpublicVariables <- function(accessBexispath = ".",
   return(paste("global variables have been set to ", "accessBexispath: ",
         accessBexispath, "pathtoRAWdata: ", pathtoRAWdata, "pathtoASSEMBLEDdata: ", pathtoASSEMBLEDdata))
 }
+
+
+
+#' Sourcing a Rmarkdown document
+#'
+#' Function extracted from : https://stackoverflow.com/questions/10966109/how-to-source-r-markdown-file-like-sourcemyfile-r
+#' Sources an Rmd file by creating a temporary R file.
+#' Aim : source Rmd files as .R files (not possible at this moment)
+#'
+#' Example use (not run) : ksource("vignettes/prepare_and_run_GDM.Rmd")
+#'
+#' @param x path to the Rmd file
+#' @return the output of the sourced file
+#' @export
+ksource <- function(x, ...) {
+  source(purl(x, output = tempfile()), ...)
+}
+
+NULL
