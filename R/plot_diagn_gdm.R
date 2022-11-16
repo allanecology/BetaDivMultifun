b6af33c20eb7a67b7bad229ccb548a0b18a4e5a7
noelle <noelle.schenk@orss.ch>
2022-11-16 18:22:34 +0100

    small update

--- R/plot_diagn_gdm.R
+++ R/plot_diagn_gdm.R
@@ -6,6 +6,7 @@
 #' plots.
 #' The function uses 200 samples along the ecological gradient to 
 #' get the line. (similar as used in gdm package.)
+#' Note that the var expl. is taken directly from the fitted model.
 #'
 #' @param x gdm model (output from gdm::gdm())
 #' @param title character string. The title to show on the plot, e.g. the model name.
@@ -19,14 +20,18 @@ plot_diagn_gdm <- function(x, title = ""){
   
   par(mfrow = c(1, 2))
   ##plots the compositional dissimilarity spline plot
-  plot(x$ecological, x$observed, xlab="Predicted Ecological Distance", ylab="Observed Compositional Dissimilarity", type="n", ylim=c(0,1), main = title)
+  plot(x$ecological, x$observed, xlab="Predicted Ecological Distance", 
+       ylab="Observed Compositional Dissimilarity", 
+       type="n", ylim=c(0,1), main = title)
   points(x$ecological, x$observed, pch=20, cex=0.25, col=plot.color)
   overlayX <- seq( from=min(x$ecological), to=max(x$ecological), length=PSAMPLE )
   overlayY <- 1 - exp( - overlayX )
   lines(overlayX, overlayY, lwd=plot.linewidth )
   
   ##plots the second compositional dissimilarity spline plot
-  plot(x$predicted, x$observed, xlab="Predicted Compositional Dissimilarity", ylab="Observed Compositional Dissimilarity", type="n", ylim=c(0,1), main = paste("%var expl =", round(x$explained, 2)))
+  plot(x$predicted, x$observed, xlab="Predicted Compositional Dissimilarity", 
+       ylab="Observed Compositional Dissimilarity", type="n", ylim=c(0,1), 
+       main = paste("%var expl =", round(x$explained, 2)))
   points( x$predicted, x$observed, pch=20, cex=0.25, col=plot.color )
   overlayX <- overlayY <- seq( from=min(x$predicted), to=max(x$predicted), length=PSAMPLE )
   lines( overlayX, overlayY, lwd=plot.linewidth )
