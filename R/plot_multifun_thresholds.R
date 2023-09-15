911afd624f005a40c7fe3cf87ce0127122e2e8a1
noelle <noelle.schenk@orss.ch>
2023-09-15 15:27:42 +0200

    updated calculation of average models. Normalisation is done per value, predictor and model type. 0 handling is better now. Plots with SE instead of SD are way easier to interpret

--- R/plot_multifun_thresholds.R
+++ R/plot_multifun_thresholds.R
@@ -20,6 +20,9 @@
 #' @param legend logical T or F, indicating if the plot or the legend should be returned.
 #' @param ymax the maximum y value shown. Defaults to 1.
 #' @param plottitle a character giving the title of the plot. Defaults to no title.
+#' @param ribbon logical. T if error bands should be shown, F otherwise.
+#' @param ribbontype character, either "se" or "sd". Defines the type of errorbands shown
+#' if ribbon is set T (i.e. error bands are shown)
 #' @return a ggplot2 plot element, with lineplots.
 #'
 #' @import ggplot2
@@ -27,13 +30,16 @@
 #' @import data.table
 #'
 #' @export
-create_gdm_lineplot_predictorwise_thresholds <- function(data, legend = F, ribbon = T, ymax = 1, plottitle = ""){
+create_gdm_lineplot_predictorwise_thresholds <- function(data, legend = F, ribbon = T, 
+                                                         ribbontype = "se", ymax = 1, plottitle = ""){
   test <- unique(data[, .(color, nicenames)])
   
   p <- ggplot(data, aes(x = xaxis, y = value, fill = model_name, color = model_name)) +
-    {if(ribbon)geom_ribbon(aes(ymin = minusSD_Y, ymax = plusSD_Y), alpha = 0.1, linetype = 0)} +
+    {if(ribbon)if(ribbontype == "sd")geom_ribbon(aes(ymin = minusSD_Y, ymax = plusSD_Y), alpha = 0.1, linetype = 0)} +
+    {if(ribbon)if(ribbontype == "se")geom_ribbon(aes(ymin = value - se, ymax = value + se), alpha = 0.1, linetype = 0)} +
     geom_line(aes(linewidth = lwd)) +
-    scale_colour_brewer(palette = "PuOr") +
+    scale_colour_brewer(palette = "PiYG") +
+    scale_fill_brewer(palette = "PiYG") +
     coord_cartesian(ylim = c(0, ymax)) +
     scale_linewidth_identity() +
     theme(axis.title = element_blank(),
